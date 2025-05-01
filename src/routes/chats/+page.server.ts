8466f54e026dfa72c100c3929c120e7b160a557f
rdtect <ar.rickde@gmail.com>
2025-05-01 09:36:03 +0530

    removing node crypto for webapi

--- src/routes/chats/+page.server.ts
+++ src/routes/chats/+page.server.ts
@@ -1,6 +1,6 @@
 import { fail, redirect } from "@sveltejs/kit";
-import type { Actions, PageServerLoad } from "./$types";
-import crypto from "crypto";
+import type { Actions } from "./$types";
+
 
 
 export const actions: Actions = {
